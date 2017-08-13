bce42f07f7ca7a01d4c190e7e12b9b07060212d2
Kunal Mehta <legoktm@member.fsf.org>
2017-08-13 11:41:03 -0400

    Make $VERSION optional

--- thing.sh
+++ thing.sh
@@ -1,7 +1,9 @@
 git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/$EXT.git --depth=1
 cd $EXT
 composer update
-composer require mediawiki/mediawiki-codesniffer $VERSION --prefer-dist
+if [ -n "$VERSION" ]; then
+    composer require mediawiki/mediawiki-codesniffer $VERSION --prefer-dist;
+fi;
 echo '------------'
 php ./vendor/bin/phpcs --report=json
 echo -e "\n"
