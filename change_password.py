067df50c8482cbd95461912223deae1e378c7fc6
Kunal Mehta <legoktm@member.fsf.org>
2017-09-02 00:55:05 -0700

    Add a script to rotate the Gerrit HTTP password
    Change-Id: I1468b99e404ede9c22caac92b1d391342a373383

---
+++ change_password.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Resets a Gerrit account's HTTP password
+Copyright (C) 2017 Kunal Mehta <legoktm@member.fsf.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import json
+import getpass
+import requests
+from requests.auth import HTTPDigestAuth
+import sys
+
+import upgrade
+
+s = requests.Session()
+
+
+def gerrit_request(method, path, **kwargs):
+    r = s.request(method, 'https://gerrit.wikimedia.org/r/a/' + path, **kwargs)
+    r.raise_for_status()
+
+    return json.loads(r.text[4:])
+
+
+def main():
+    pw = getpass.getpass('HTTP Password for %s: ' % upgrade.GERRIT_USER)
+    auth = HTTPDigestAuth('libraryupgrader', pw)
+    # Check that we're logged in as the right user
+    account = gerrit_request('GET', path='accounts/self', auth=auth)
+    if account['username'] != upgrade.GERRIT_USER:
+        print('Error, logged in as %(username)s (%(email)s)??' % account)
+        sys.exit(1)
+    print('Successfully logged in as %(username)s' % account)
+    new_password = gerrit_request(
+        'PUT',
+        path='accounts/self/password.http',
+        auth=auth,
+        data=json.dumps({'generate': True}),
+        headers={'Content-Type': 'application/json'}
+    )
+    print('The following is your new HTTP password, please save it:')
+    print('----------')
+    print(new_password)
+    print('----------')
+
+
+if __name__ == '__main__':
+    main()
