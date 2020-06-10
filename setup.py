4b803d7096040acd76e9ff92cdf964d6e7461913
Kunal Mehta <legoktm@member.fsf.org>
2020-06-10 01:10:23 -0700

    Revert "Get rid of unused setup.py"
    This reverts commit f42315f347d54b2cc544c7369a265f5bbe6b3d16.
    
    Change-Id: I38d4a876d73c421c4da779e030b5f30d3f9ef37f

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='libup',
+    version='0.0.1',
+    packages=['libup'],
+    url='https://www.mediawiki.org/wiki/Libraryupgrader',
+    license='AGPL-3.0-or-later',
+    author='Kunal Mehta',
+    author_email='legoktm@member.fsf.org',
+    description='semi-automated tool that manages upgrades of libraries',
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'libup-run = libup.run:main',
+            'libup-ng = libup.ng:main',
+        ]
+    }
+
+)
